b9fa3b1a73b582745a9e60be563496d62cba89db
ffernandezpolitecnics <ffernandez@politecnics.barcelona>
2022-12-01 18:13:42 +0100

    config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/pokemon-prod/',
+  publicPath: '/04-pokemon-game/',
 })
